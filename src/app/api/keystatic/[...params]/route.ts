6790d3e59d416c1b84d6126ab98ab0a1d90e4487
wimxwim <wimxgooo@gmail.com>
2026-06-18 08:27:06 +0700

    debug: add console.log to trace X-Forwarded-Host rewrite in route.ts

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -4,25 +4,32 @@ import config from "../../../../../keystatic.config";
 const { GET: keystaticGET, POST: keystaticPOST } = makeRouteHandler({ config });
 
 function rewriteRequest(request: Request): Request {
-  const url = new URL(request.url);
+  const originalUrl = request.url;
   const forwardedHost = request.headers.get("x-forwarded-host");
   const forwardedProto = request.headers.get("x-forwarded-proto");
 
-  if (forwardedHost && url.hostname !== forwardedHost) {
+  console.log("[keystatic-route] original URL:", originalUrl);
+  console.log("[keystatic-route] x-forwarded-host:", forwardedHost);
+  console.log("[keystatic-route] x-forwarded-proto:", forwardedProto);
+
+  if (forwardedHost) {
+    const url = new URL(request.url);
     url.hostname = forwardedHost;
     if (forwardedProto) url.protocol = forwardedProto + ":";
-    return new Request(url.toString(), request);
+    const newUrl = url.toString();
+    console.log("[keystatic-route] rewritten URL:", newUrl);
+    return new Request(newUrl, request);
   }
 
   return request;
 }
 
-export const GET = async (request: Request) => {
+export const GET = async (request: Request, context: any) => {
   const rewritten = rewriteRequest(request);
   return keystaticGET(rewritten);
 };
 
-export const POST = async (request: Request) => {
+export const POST = async (request: Request, context: any) => {
   const rewritten = rewriteRequest(request);
   return keystaticPOST(rewritten);
 };
